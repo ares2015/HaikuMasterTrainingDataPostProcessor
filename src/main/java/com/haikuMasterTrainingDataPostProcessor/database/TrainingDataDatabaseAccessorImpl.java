2dd86709a988cb79f5c13e171c1bf3c5a0cb642b
Oliver Eder <eder@essential-data.sk>
2017-01-28 15:56:00 +0100

    Added sql statement into insert method.

--- src/main/java/com/haikuMasterTrainingDataPostProcessor/database/TrainingDataDatabaseAccessorImpl.java
+++ src/main/java/com/haikuMasterTrainingDataPostProcessor/database/TrainingDataDatabaseAccessorImpl.java
@@ -23,5 +23,50 @@ public class TrainingDataDatabaseAccessorImpl implements TrainingDataDatabaseAcc
     @Override
     public void insertTokenWord2VecData(String keyToken, List<TokenVectorData> tokenVectorDataList) {
         List<String> databaseList = databaseListFactory.create(tokenVectorDataList);
+        String sql = "insert into jos_haiku_master_word2vec_model (token, " +
+                "neighbour1," +
+                "neighbour2," +
+                "neighbour3," +
+                "neighbour4," +
+                "neighbour5," +
+                "neighbour6," +
+                "neighbour7," +
+                "neighbour8," +
+                "neighbour9," +
+                "neighbour10) " +
+                "neighbour11) " +
+                "neighbour12) " +
+                "neighbour13) " +
+                "neighbour14) " +
+                "neighbour15) " +
+                "neighbour16) " +
+                "neighbour17) " +
+                "neighbour18) " +
+                "neighbour19) " +
+                "neighbour20) " +
+                "values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        jdbcTemplate.update(sql, new Object[]{keyToken,
+                databaseList.get(0),
+                databaseList.get(1),
+                databaseList.get(2),
+                databaseList.get(3),
+                databaseList.get(4),
+                databaseList.get(5),
+                databaseList.get(6),
+                databaseList.get(7),
+                databaseList.get(8),
+                databaseList.get(9),
+                databaseList.get(10),
+                databaseList.get(11),
+                databaseList.get(12),
+                databaseList.get(13),
+                databaseList.get(14),
+                databaseList.get(15),
+                databaseList.get(16),
+                databaseList.get(17),
+                databaseList.get(18),
+                databaseList.get(19),
+                databaseList.get(20)
+        });
     }
 }
